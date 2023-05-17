e28c3ffa46da09de9ff4298f873bfa9683226f9b
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-05-17 10:22:46 +0300

    useDrop

--- src/DragAndDrop-Component/Bin.jsx
+++ src/DragAndDrop-Component/Bin.jsx
@@ -1,4 +1,14 @@
+import { useDrop } from "react-dnd"
+
 const Bin = () => {
+	const [{ canDrop, isOver }, drop] = useDrop(() => ({
+		accept: 'note',
+		drop: () => ({ name: 'the bin' }),
+		collect: monitor => ({
+			isOver: !!monitor.isOver(),
+			canDrop: !!monitor.canDrop(),
+		})
+	}), [],)
 	return (
 		<div className="bin">Bin</div>
 	)
