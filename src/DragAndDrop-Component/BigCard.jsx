a5517c0c20b8fae7944f49f7d21171c4ec910e6c
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-05-17 10:39:05 +0300

    useDrag carrently it is work onle for desktops

--- src/DragAndDrop-Component/BigCard.jsx
+++ src/DragAndDrop-Component/BigCard.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { useDrag } from 'react-dnd'
 
 const Card = ({ id, text }) => {
+	const [{ isDragging }, drag] = useDrag(() => ({
+		type: 'note',
+		//item: { id, text },
+		item: { name: text },
+		end: (item, monitor) => {
+			const dropResult = monitor.getDropResult()
+			if (item && dropResult) {
+				alert(`You threw ${item.name} into ${dropResult.name}`)
+			}
+		},
+		collect: (monitor) => ({
+			isDragging: !!monitor.isDragging(),
+		})
+	}), [],)
+
+
 	return (
-		<div id={id}
-			className='card'>{text}</div>
+		<div id={id} ref={drag}
+			className='card' >{text}</div>
 	)
 }
 
