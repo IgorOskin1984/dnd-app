28eeddbc51aaa3471f7aac86dac8b24147a5e142
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-05-23 18:27:45 +0300

    заменил все на работающий вариант

--- src/App.js
+++ src/App.js
@@ -1,49 +1,58 @@
-import './App.css';
-import { useCallback, useState } from 'react';
-import { DnDBoards } from './DragAndDrop-Component/DnD_Boards';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import Card from './DragAndDrop-Component/BigCard';
+import { useCallback, useState } from 'react'
+import { Card } from './Card.js'
+import './App.css'
 
+export const Container = () => {
 
-function App() {
-
-	const [cardList, setCardList] = useState([
-		{ id: 4, order: 4, text: 'Card 4' },
-		{ id: 3, order: 3, text: 'Card 3' },
-		{ id: 2, order: 2, text: 'Card 2' },
-		{ id: 1, order: 1, text: 'Card 1' },
-	]);
-
-	const moveCard = useCallback(() => { }, [])
-
-	const renderCard = useCallback((card, i) => {
+	const [cards, setCards] = useState([
+		{
+			id: 1,
+			text: 'Write a cool JS library',
+		},
+		{
+			id: 2,
+			text: 'Make it generic enough',
+		},
+		{
+			id: 3,
+			text: 'Write README',
+		},
+		{
+			id: 4,
+			text: 'Create some examples',
+		},
+		{
+			id: 5,
+			text: 'Spam in Twitter and IRC to promote it (note that this element is taller than the others)',
+		},
+		{
+			id: 6,
+			text: '???',
+		},
+		{
+			id: 7,
+			text: 'PROFIT',
+		},
+	])
+	const moveCard = useCallback((dragIndex, hoverIndex) => {
+		setCards(prevCards => {
+			const updatedCards = [...prevCards];
+			[updatedCards[dragIndex], updatedCards[hoverIndex]] = [updatedCards[hoverIndex], updatedCards[dragIndex]];
+			return updatedCards;
+		});
+	}, []);
+	const renderCard = useCallback((card, index) => {
 		return (
 			<Card
-				key={new Date().getTime() + Math.floor(Math.random() * 1000)}
+				key={card.id}
+				index={index}
 				id={card.id}
-				order={card.order}
 				text={card.text}
-				cardList={cardList}
 				moveCard={moveCard}
 			/>
 		)
 	}, [])
-
-	const sortCardList = (a, b) => a.order - b.order;
-
 	return (
-		<DndProvider backend={HTML5Backend}>
-			<div className='App'>
-				<div className="wrapper">
-					{/*<DnDBoards />*/}
-					<div className='body'>
-						{cardList.sort(sortCardList).map((card, i) => renderCard(card, i))}
-					</div>
-				</div>
-			</div>
-		</DndProvider>
-	);
+		<div className='body' >{cards.map((card, i) => renderCard(card, i))}</div>
+	)
 }
-
-export default App;
