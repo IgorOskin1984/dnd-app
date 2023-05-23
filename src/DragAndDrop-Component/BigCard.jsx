606a39bb1e67cde7471ae3a0403009a3dd0ce491
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-05-23 18:28:24 +0300

    заменил все во втором файле

--- src/DragAndDrop-Component/BigCard.jsx
+++ src/DragAndDrop-Component/BigCard.jsx
@@ -1,63 +1,56 @@
-import React, { useRef } from 'react'
+import { useRef } from 'react'
 import { useDrag, useDrop } from 'react-dnd'
+const style = {
 
-const Card = ({ id, order, text, cardList, moveCard }) => {
+}
+
+export const Card = ({ id, text, index, moveCard }) => {
 	const ref = useRef(null)
-	//========================================================================================================================================================
-
-	const hoverHandle = (item, monitor) => {
-		if (!ref.current) return
-		//debugger
-		const dropOrderFunc = () => {
-			let targetItem = cardList.find((item) => item.id === parseInt(ref.current.id));
-			return targetItem.order
-		}
-		const dropOrder = dropOrderFunc()
-
-		if (item.order === dropOrder) {
-			return
-		}
-		const hoverBoundingRect = ref.current?.getBoundingClientRect()
-		const hoverMiddleX = (hoverBoundingRect.right - hoverBoundingRect.left) / 2;
-		const clientOffset = monitor.getClientOffset()
-		const hoverClientX = clientOffset.x - hoverBoundingRect.left;
-		if (item.order < dropOrder && hoverClientX < hoverMiddleX) {
-			return
-		}
-		if (item.order > dropOrder && hoverClientX > hoverMiddleX) {
-			return
-		}
-		moveCard(item.order, dropOrder);
-
-
-		item.order = dropOrder;
-		console.log(item);
-
-	}
-	//========================================================================================================================================================
-
-	const [{ handlerId, isOver }, drop] = useDrop(() => ({
+	const [{ handlerId, isOver }, drop] = useDrop({
 		accept: 'card',
+		//!===
 		collect(monitor) {
 			return {
 				handlerId: monitor.getHandlerId(),
-				isOver: monitor.isOver(),
+				isOver: monitor.isOver()
 			}
 		},
-		hover: hoverHandle
-	}))
+		//!===
+		hover(item, monitor) {
+			if (!ref.current) {
+				return
+			}
+			const dragIndex = item.index
+			const hoverIndex = index
 
-	const [{ isDragging }, drag] = useDrag(() => ({
+			if (dragIndex === hoverIndex) {
+				return
+			}
+			const hoverBoundingRect = ref.current?.getBoundingClientRect()
+			const hoverMiddleX = (hoverBoundingRect.right - hoverBoundingRect.left) / 2
+			const clientOffset = monitor.getClientOffset()
+			const hoverClientX = clientOffset.x - hoverBoundingRect.left;
+
+			if (dragIndex < hoverIndex && hoverClientX < hoverMiddleX) {
+				return
+			}
+			if (dragIndex > hoverIndex && hoverClientX > hoverMiddleX) {
+				return
+			}
+			moveCard(dragIndex, hoverIndex)
+
+			item.index = hoverIndex
+		},
+	})
+	const [{ isDragging }, drag] = useDrag({
 		type: 'card',
 		item: () => {
-			return { id, order, text }
+			return { id, index }
 		},
-		//item: { name: text },
 		collect: (monitor) => ({
 			isDragging: monitor.isDragging(),
-		})
-	}), [],)
-
+		}),
+	})
 
 	const className = () => {
 		if (isDragging) {
@@ -71,9 +64,8 @@ const Card = ({ id, order, text, cardList, moveCard }) => {
 
 	drag(drop(ref))
 	return (
-		<div id={id} ref={ref} data-handler-id={handlerId}
-			className={className()} >{text}</div>
+		<div className={className()} ref={ref} data-handler-id={handlerId}>
+			{text}
+		</div>
 	)
 }
-
-export default Card
