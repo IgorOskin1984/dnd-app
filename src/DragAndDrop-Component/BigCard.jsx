7d6c21dafcca0c17ee35dd40e863de5e74d73a37
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-05-24 11:01:04 +0300

    забрал ховер из рабочего варианта. баг не ушел

--- src/DragAndDrop-Component/BigCard.jsx
+++ src/DragAndDrop-Component/BigCard.jsx
@@ -22,28 +22,31 @@ export const Card = ({ id, text, order, apdateState }) => {
 				isOver: monitor.isOver()
 			}
 		},
-		hover: (item, monitor) => {
-			const dragOrder = item.order;
-			const hoverOrder = order;
-			console.log(dragOrder, hoverOrder);
+		hover(item, monitor) {
+			if (!ref.current) {
+				return
+			}
+			const dragOrder = item.order
+			const hoverOrder = order
 
 			if (dragOrder === hoverOrder) {
 				return
 			}
 			const hoverBoundingRect = ref.current?.getBoundingClientRect()
-			const hoverMiddleX = (hoverBoundingRect.right - hoverBoundingRect.left) / 2;
+			const hoverMiddleX = (hoverBoundingRect.right - hoverBoundingRect.left) / 2
 			const clientOffset = monitor.getClientOffset()
 			const hoverClientX = clientOffset.x - hoverBoundingRect.left;
+
 			if (dragOrder < hoverOrder && hoverClientX < hoverMiddleX) {
 				return
 			}
 			if (dragOrder > hoverOrder && hoverClientX > hoverMiddleX) {
 				return
 			}
+			apdateState(dragOrder, hoverOrder)
 
-			apdateState(dragOrder, hoverOrder);
-			item.order = hoverOrder;
-		}
+			item.order = hoverOrder
+		},
 	}))
 	//========================================================================================================================================================
 
