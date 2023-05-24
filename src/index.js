2f2ff61e3215833b61230a8245dd2c0b73a58206
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-05-24 12:59:22 +0300

    Добавил ЧАТ-код в index.js на десктоп все работает

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { TouchBackend } from 'react-dnd-touch-backend';
 import App from './App';
+
+const isTouchDevice = 'ontouchstart' in window;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<DndProvider backend={HTML5Backend}>
+		<DndProvider backend={isTouchDevice ? TouchBackend : HTML5Backend}>
 			<App />
 		</DndProvider>
 	</React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
